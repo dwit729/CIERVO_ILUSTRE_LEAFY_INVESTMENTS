3d0211dafeecc8de061473c9ea97597e519a2aa1
Sofia <ilustresg@students.nu-moa.edu.ph>
2024-01-29 22:35:33 +0800

    Test Commit

--- app/src/main/java/com/example/ciervo_ilustre_leafy_investments/MainActivity.java
+++ app/src/main/java/com/example/ciervo_ilustre_leafy_investments/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.ciervo_ilustre_leafy_investments;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textView);
+
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText("Nagana na sakin.");
+            }
+        });
     }
 }
