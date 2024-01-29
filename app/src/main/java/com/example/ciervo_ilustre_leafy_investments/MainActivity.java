7c67228bf4e75966c5014ecaec6fdc910b3024e7
dwit729 <124338784+dwit729@users.noreply.github.com>
2024-01-29 22:09:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ciervo_ilustre_leafy_investments/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ciervo_ilustre_leafy_investments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CIERVO_ILUSTRE_LEAFY_INVESTMENTS"
+include(":app")
+ 
